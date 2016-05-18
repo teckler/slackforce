a9449ecadadc18e8bbfac9442caf95ae5ce8408f
teckler <teckler@users.noreply.github.com>
2016-05-18 15:50:53 -0400

    Update opportunity.js

--- modules/opportunity.js
+++ modules/opportunity.js
@@ -12,7 +12,7 @@ function execute(req, res) {
     var limit = req.body.text;
     if (!limit || limit=="") limit = 5;
 
-    var q = "SELECT Id, Name, Amount, Probability, StageName, CloseDate FROM Opportunity where isClosed=false ORDER BY amount DESC LIMIT " + limit;
+    var q = "SELECT Id, Name, format(Amount), Probability, StageName, CloseDate FROM Opportunity where isClosed=false ORDER BY CloseDate DESC LIMIT " + limit;
     org.query({query: q}, function(err, resp) {
         if (err) {
             console.error(err);
